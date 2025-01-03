1778f8707569b6993a96a145b21e4b3a463ef188
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-03 14:47:36 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -10,28 +10,12 @@ $(function () {
     dots: true,
     centerMode: true,
     cssEase: 'linear',
-    prevArrow: '.gallery__list-left',
-    nextArrow: '.gallery__list-right',
+
   });
 });
 
 
-$(function () {
-  $('.features-list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: true,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.features__list-left',
-    nextArrow: '.features__list-right',
-  });
-});
+
 $(function () {
   $('.advantages-list').slick({
     infinite: true,
@@ -49,22 +33,7 @@ $(function () {
   });
 });
 
-$(function () {
-  $('.gallery-diary__list').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    speed: 1000,
-    dots: true,
-    centerMode: true,
-    cssEase: 'linear',
-    prevArrow: '.gallery-diary__list-left',
-    nextArrow: '.gallery-diary__list-right',
-  });
-});
+
 
 
 
